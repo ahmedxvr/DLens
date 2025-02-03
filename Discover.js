6118231ee5460586aa20e9ff905b0fb4a47b76e8
Ahmed_Adil <ahmed_adil@edwards.com>
2025-02-03 16:34:46 +0500

    Modifying Discover

--- Discover.js
+++ Discover.js
@@ -10,37 +10,51 @@ const url = 'mongodb://ahmed:XvrLhr123@discoverydocdb.cluster-cl4o62qemt92.eu-we
 // Database Name
 const dbName = 'discoverydocdb';
 
+// const dbName = 'myproject';
+const client = new MongoClient(url, { tlsCAFile: `global-bundle.pem` });
+
+// Use connect method to connect to the server
+client.connect(function (err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    insertDocuments(db, function () {
+        client.close();
+    });
+});
 
 //Create a MongoDB client, open a connection to DocDB; as a replica set,
 //  and specify the read preference as secondary preferred
 
-MongoClient.connect(
-    'mongodb://ahmed:XvrLhr123@discoverydocdb.cluster-cl4o62qemt92.eu-west-2.docdb.amazonaws.com:27017/?directConnection=true&tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',
-    {
-        tlsCAFile: `global-bundle.pem` //Specify the DocDB; cert
-    },
-    function (err, client) {
-        if (err)
-            throw err;
+// MongoClient.connect(
+//     'mongodb://ahmed:XvrLhr123@discoverydocdb.cluster-cl4o62qemt92.eu-west-2.docdb.amazonaws.com:27017/?directConnection=true&tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',
+//     {
+//         tlsCAFile: `global-bundle.pem` //Specify the DocDB; cert
+//     },
+//     function (err, client) {
+//         if (err)
+//             throw err;
 
-        //Specify the database to be used
-        db = client.db(dbName);
+//         //Specify the database to be used
+//         db = client.db(dbName);
 
-        //Specify the collection to be used
-        col = db.collection('sample-collection');
+//         //Specify the collection to be used
+//         col = db.collection('sample-collection');
 
-        //Insert a single document
-        col.insertOne({ 'hello': 'Amazon DocumentDB' }, function (err, result) {
-            //Find the document that was previously written
-            col.findOne({ 'hello': 'Amazon DocumentDB' }, function (err, result) {
-                //Print the result to the screen
-                console.log(result);
+//         //Insert a single document
+//         col.insertOne({ 'hello': 'Amazon DocumentDB' }, function (err, result) {
+//             //Find the document that was previously written
+//             col.findOne({ 'hello': 'Amazon DocumentDB' }, function (err, result) {
+//                 //Print the result to the screen
+//                 console.log(result);
 
-                //Close the connection
-                client.close()
-            });
-        });
-    });
+//                 //Close the connection
+//                 client.close()
+//             });
+//         });
+//     });
 
 
 // async function importData() {
